a359601e838b208ffbea71ab8bcc1c960500ed88
phal sopha <sophavathana@gmail.com>
2024-05-29 16:36:47 +0700

    Second commit  kam

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h2>Hello world </h2>
+    <button @click="increment"> Increment</button>
+    <button @click="decrement"> decrement</button>
+    <p>{{count}}</p>
   </div>
 </template>
+
+
+<script setup>
+const count =ref(0)
+function increment() {
+  count.value++;
+  }
+function decrement() {
+  count.value--;
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
